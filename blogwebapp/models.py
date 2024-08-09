4803fdaeef0ae68ea1a0c49f4938437a69b0e142
BenardMN <benardmunuve934@gmail.com>
2024-08-09 08:15:37 +0300

    Add conn to mssql, add view issues

--- blogwebapp/models.py
+++ blogwebapp/models.py
@@ -13,6 +13,39 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class CMIssuesHeader(models.Model):
+    id = models.IntegerField(primary_key=True)
+    MIssueNo = models.CharField(max_length=50)
+    MDate = models.CharField(max_length=50)
+    MDescription = models.CharField(max_length=50)
+    MComment = models.CharField(max_length=50)
+    Reference = models.CharField(max_length=50)
+    IssueNo = models.CharField(max_length=50)
+    Posted = models.IntegerField()
+    LastUser = models.CharField(max_length=50)
+    LastMaintained = models.CharField(max_length=50)
+    SubLocation = models.CharField(max_length=50)
+    Contractor = models.CharField(max_length=50)
+    MSite = models.CharField(max_length=50)
+    TicketNo = models.CharField(max_length=50)
+    IssueType = models.CharField(max_length=50)
+    UserDepartment = models.CharField(max_length=50)
+    SDUManager = models.CharField(max_length=50)
+    ProcurementManager = models.CharField(max_length=50)
+    CTO = models.CharField(max_length=50)
+    RequestedBy = models.CharField(max_length=50)
+    Destination = models.CharField(max_length=50)
+    DeliveryDate = models.CharField(max_length=50)
+    # MProject = models.CharField(max_length=50)
+    # MYear = models.CharField(max_length=50)
+    # MTask = models.CharField(max_length=50)
+    # BTS = models.CharField(max_length=50)
+    RequestedByTime = models.CharField(max_length=50)
+    SDUManagerTime = models.CharField(max_length=50)
+    ProcurementManagerTime = models.CharField(max_length=50)
+    PostedToInventoryTime  = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.MIssueNo
 
 
--- blogwebapp/urls.py
+++ blogwebapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import update_issue
 #from . import views
 urlpatterns = [
     path('', views.home, name = 'blog-home'),
     path('about/', views.about, name = 'blog-about'),
+    path('issues/', views.issues, name = 'blog-issues'),
+    path('update_issue/<int:issue_id>/', update_issue, name='update_issue'),
 ]
